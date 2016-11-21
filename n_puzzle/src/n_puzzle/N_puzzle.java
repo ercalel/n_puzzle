4773f8049591da65c2e78178da4a9cfad2c55feb
Elmer Calel <ercalel@gmail.com>
2016-11-21 00:41:29 -0600

    A partir de esta versión se harán las optimizaciones y mejoras de la versión anterior de código.

---
+++ n_puzzle/src/n_puzzle/N_puzzle.java
@@ -0,0 +1,16 @@
+package n_puzzle;
+
+/**
+ *
+ * @author Elmer C. Ramos
+ */
+public class N_puzzle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
